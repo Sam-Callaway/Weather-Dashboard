31639a0460e91aaef0d449872d629876efc56cd3
Sam Callaway <calls002@gmail.com>
2023-02-24 18:31:51 +0000

    mixing up API key

---
+++ script.js
@@ -0,0 +1,22 @@
+// Obfuscate key so it's less easy for bots trawling GitHub to read
+var bue = "";
+var vvg = "";
+for (let i = 5; i > -1; i--){
+    let string = "a0e92b"
+    bue = bue + string.charAt(i)
+    vvg = "e4395c"
+}
+bue = bue.toString().substring(0,5)
+var fsh = ((2*3)+"f"+(65*5));
+var prw = "a" + (10299-842+90)
+var kfl = "";
+for (let i = 5; i > -1; i--){
+    let string = "f25f4"
+    kfl = kfl + string.charAt(i)
+}
+
+var sre = "f2b"+"c3f"
+
+const abc = fsh+bue+prw+kfl+sre+vvg
+
+
